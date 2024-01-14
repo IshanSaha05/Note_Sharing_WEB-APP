03aa5e0e03344c2ca1fdd3464b1fa867f8407a42
Ishan Saha <63413315+IshanSaha05@users.noreply.github.com>
2024-01-14 16:40:33 +0530

    Delete pkg directory

--- pkg/models/notes_model.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type NoteData struct {
-	Email      *string `json:"email"`
-	Data       *string `json:"notesData"`
-	Sharable   bool    `json:"sharable"`
-	Created_At *string `json:"createdAt"`
-	Updated_At *string `json:"updatedAt"`
-}
